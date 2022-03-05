f2c7568c3cab8568ec38dd71a109441a06482348
Nell B <nellb28@yahoo.com>
2022-03-05 08:54:24 -0600

    Add initial classroom version

--- constraint-validation.js
+++ constraint-validation.js
@@ -1,1 +1,17 @@
 // TODO
+const firstNameField = document.getElementById("first-name");
+const form = document.getElementById("connect-form");
+
+const checkValidation = (e) => {
+  console.log(firstNameField.value.length);
+  const validState = firstNameField.value.length > 3;
+  if (!validState) {
+    e.preventDefault();
+    firstNameField.setCustomValidity("THIS IS GREAT!");
+  } else {
+    firstNameField.validity.valid = true;
+  }
+};
+
+form.addEventListener("submit", checkValidation);
+//TODO - there is an issue when we refresh the page. Need to investigate this
