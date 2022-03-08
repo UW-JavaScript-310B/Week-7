45163607346e91321e6bed5f92214f3c4c75f250
JCahill <cahilljm@aol.com>
2022-03-08 11:52:53 -0800

    Separate constraint validations into separate functions

--- constraint-validation.js
+++ constraint-validation.js
@@ -1,8 +1,10 @@
 const firstName = document.getElementById("first-name");
 const lastName = document.getElementById("last-name");
 const form = document.getElementById("connect-form");
-const submitButton = document.querySelector(".btn-primary")
 const emailAddr = document.getElementById("email");
+
+const inputValue = document.getElementsByTagName("input")
+const submitButton = document.querySelector(".btn-primary")
 
 const parentDivFirstName = firstName.parentElement
 const parentDivLastName = lastName.parentElement
@@ -14,7 +16,9 @@ const parentDivEmail = emailAddr.parentElement
 // console.log(emailAddr);
 // console.log(firstName.closest(".form-group"))
 // console.log(firstName)
+// console.log(inputValue)
 
+//hide and show fields depending on selection of interest
 const selector = document.getElementById("contact-kind");
 
 const selectOption = () => {
@@ -24,14 +28,11 @@ const selectOption = () => {
         const bizfields = document.getElementById("biz-name");
         bizfields.classList.remove("hidden");
         bizfields.classList.add("show");
-    } 
-    else if (selection === "technical") {
+    } else if (selection === "technical") {
         const techfields = document.getElementById("tech-type");
         techfields.classList.remove("hidden");
         techfields.classList.add("show");
-    } 
-    
-    else {
+    } else {
         bizfields.classList.add("hidden");
         techfields.classList.add("hidden");
 
@@ -39,61 +40,78 @@ const selectOption = () => {
 
 }
 
-const validateForm = (e) => {
-    const validFirstName = firstName.value.length >= 3;
-    const validLastName = lastName.value.length >= 3;
+//start of field validations
 
-    //validate that first and last name are at least 3 characters long
+//validate first name
+function validateFirstName() {
+    const validFirstName = firstName.value.length >= 3;
+
     if (!validFirstName) {
-        e.preventDefault();
         console.log("Bad Input");
         parentDivFirstName.classList.remove("valid");
         parentDivFirstName.classList.add("invalid");
         firstName.setCustomValidity("Your first name must include at least 3 characters");
+        firstName.validity.valid = false;
         firstName.reportValidity();
-    } else if (!validLastName) {
-        e.preventDefault();
+    } else {
+        parentDivFirstName.classList.remove("invalid");
+        parentDivFirstName.classList.add("valid");
+        firstName.setCustomValidity("");
+        firstName.validity.valid = true;
+        firstName.reportValidity();
+    }
+}
+
+//validate last name
+function validateLastName() {
+    const validLastName = lastName.value.length >= 3;
+
+    if (!validLastName) {
         console.log("Bad Input");
         parentDivLastName.classList.remove("valid");
         parentDivLastName.classList.add("invalid");
         lastName.setCustomValidity("Your last name must include at least 3 characters");
+        lastName.validity.valid = false;
         lastName.reportValidity();
     } else {
-        parentDivFirstName.classList.remove("invalid");
         parentDivLastName.classList.remove("invalid");
-        parentDivFirstName.classList.add("valid");
         parentDivLastName.classList.add("valid");
-        firstName.validity.valid = true;
+        lastName.setCustomValidity("");
         lastName.validity.valid = true;
+        lastName.reportValidity();
     }
+}
 
+//validate email address
+function validateEmail() {
+    const regex = /\w+@\w+\.\w+/;
 
-    // validate whether email matches regex format
+    if (emailAddr.value.match(regex)) {
+        parentDivEmail.classList.remove("invalid");
+        parentDivEmail.classList.add("valid");
+        emailAddr.setCustomValidity("");
+        emailAddr.validity.valid = true;
+        emailAddr.reportValidity();
+        // return ('true');
+    } else {
+        console.log("Bad Input");
+        parentDivEmail.classList.remove("valid");
+        parentDivEmail.classList.add("invalid");
+        emailAddr.setCustomValidity("Your email address is not valid");
+        emailAddr.validity.valid = false;
+        emailAddr.reportValidity();
+        // return ('false');
+    }
+}
 
-       const regex = /\w+@\w+\.\w+/;
+//function to call field validations
+const validateForm = (e) => { 
+    validateFirstName();
+    validateLastName();
+    validateEmail();
+}
 
-        if (emailAddr.value.match(regex)) {
-            parentDivEmail.classList.remove("invalid");
-            parentDivEmail.classList.add("valid");
-            emailAddr.setCustomValidity("Your email address is valid");
-            return ('true');
-        } else {
-            e.preventDefault();
-            console.log("Bad Input");
-            parentDivEmail.classList.remove("valid");
-            parentDivEmail.classList.add("invalid");
-            emailAddr.setCustomValidity("Your email address is not valid");
-            emailAddr.reportValidity();
-            return ('false');
-
-
-        }
-};
-
-
-
-
-
+//trigger on button click to stop edits until all fields are entered
 submitButton.addEventListener("click", validateForm);
 
 selector.addEventListener("change", selectOption)
