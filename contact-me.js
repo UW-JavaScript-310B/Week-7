492582861c1c48cde4bf7973eb9f539efa45ace8
Admin <admin@Admins-MacBook-Pro.local>
2022-03-09 11:20:01 -0800

    Removed validity.valid from JS to prevent user from bypassing validation by editing HTML.

--- contact-me.js
+++ contact-me.js
@@ -1,4 +1,3 @@
-// TODO
 const form = document.querySelector('form');
 const firstNameField = document.getElementById('first-name');
 const lastNameField = document.getElementById('last-name');
@@ -9,64 +8,92 @@ const companyUrlField = document.getElementById('company-url');
 const codingLanguageField = document.getElementById('coding-language');
 const messageField = document.getElementById('message');
 
-const localNameValidation = e => e.target.value.length < 3 ? invalidateField(e) : validateField(e);
-
-const localEmailValidation = e => e.target.value.match(/\w+@\w+\.\w+/) ? validateField(e) : invalidateField(e);
-
-const selectUpdate = () => {
-  console.log(reasonField.value);
-  if(reasonField.value === 'job') {
-    jobTitleField.parentElement.classList.remove('hidden');
-    companyUrlField.parentElement.classList.remove('hidden');
-    codingLanguageField.parentElement.classList.add('hidden');
-  }
-  else if(reasonField.value === 'code') {
-    codingLanguageField.parentElement.classList.remove('hidden');
-    jobTitleField.parentElement.classList.add('hidden');
-    companyUrlField.parentElement.classList.add('hidden');
-  }
-}
-
-const validateField = (e) => {
-  e.target.classList.remove('invalid');
-  e.target.classList.add('valid');
-  e.target.setCustomValidity('');
-  e.target.validity.valid = true;
-}
-
-const invalidateField = (e) => {
-  e.target.classList.remove('valid');
-  e.target.classList.add('invalid');
-  e.target.setCustomValidity(`${(e.target.name.charAt(0).toUpperCase() + e.target.name.slice(1)).replace('-', ' ')} must be three characters or more`);
-  e.target.reportValidity();
-}
-
+/**
+ * Upon submit event, validate first name, last name, email, and textarea fields
+ * @param {Event object} e 
+ */
 const submissionValidation = (e) => {
-  console.log(lastNameField.validity)
-  if(!firstNameField.validity.valid) {
+  if(firstNameField.value.length < 3) {
     firstNameField.setCustomValidity(`A minimum of three characters are required`);
     firstNameField.reportValidity();
     e.preventDefault();
   }
+  else {
+    firstNameField.setCustomValidity('');
+    firstNameField.reportValidity();
+  }
 
-  if(!lastNameField.validity.valid) {
+  if(lastNameField.value.length < 3) {
     lastNameField.setCustomValidity(`A minimum of three characters are required`);
     lastNameField.reportValidity();
     e.preventDefault();
   }
+  else {
+    lastNameField.setCustomValidity('');
+    lastNameField.reportValidity();
+  }
 
-  if(!emailField.validity.valid) {
+  if(!emailField.value.match(/\w+@\w+\.\w+/)) {
     emailField.setCustomValidity(`Please enter a valid email address`);
     emailField.reportValidity();
     e.preventDefault();
   }
+
+  if(messageField.value.length < 10) {
+    emailField.setCustomValidity(`A minimum of 10 characters is required`);
+    emailField.reportValidity();
+    e.preventDefault();
+  }
+  form.reportValidity();
 }
 
-// real-time validation of forms fields upon chan ge event
-firstNameField.addEventListener('change', localNameValidation);
-lastNameField.addEventListener('change', localNameValidation);
-emailField.addEventListener('change', localEmailValidation);
-reasonField.addEventListener('change', selectUpdate)
+/**
+ * Provides real-time form field feedback via setting custom validity messaging
+ * @param {Event object} e 
+ */
+const formRealTimeFeedback = (e) => {
+  // set "changed" class to utilize associated CSS styles
+  if(e.target.localName !== 'select') {
+    e.target.classList.add('changed');
+  }
+  
+  if(e.target.name.includes('name') && e.target.value.length < 3) {
+    e.target.setCustomValidity(`A minimum of three characters are required`);
+  }
+  else if(e.target.name.includes('email') && !(/\w+@\w+\.\w+/).test(e.target.value)) {
+    e.target.setCustomValidity(`Please enter a valid email address`);
+  }
+  else if(e.target.name.includes('message') && e.target.value.length < 10) {
+    e.target.setCustomValidity(`A minimum of ten characters are required`);
+  }
+  else {
+    e.target.setCustomValidity('');
+  }
+  e.target.reportValidity();
+}
 
-// validation of form fields upon submit event
+/**
+ * Toggles hidden form fields when parent select options are chosen
+ */
+ const selectUpdate = () => {
+  if(reasonField.value === 'job') {
+    jobTitleField.required = true;
+    jobTitleField.parentElement.classList.remove('hidden');
+    companyUrlField.required = true;
+    companyUrlField.parentElement.classList.remove('hidden');
+    codingLanguageField.required = false;
+    codingLanguageField.parentElement.classList.add('hidden');
+  }
+  else if(reasonField.value === 'code') {
+    codingLanguageField.required = true;
+    codingLanguageField.parentElement.classList.remove('hidden');
+    jobTitleField.required = false;
+    jobTitleField.parentElement.classList.add('hidden');
+    companyUrlField.required = false;
+    companyUrlField.parentElement.classList.add('hidden');
+  }
+}
+
+form.addEventListener('input', formRealTimeFeedback);
+reasonField.addEventListener('change', selectUpdate)
 form.addEventListener('submit', submissionValidation);
