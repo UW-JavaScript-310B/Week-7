bf60968ada661c2ed55e836cd8c94e51ca061cfe
JCahill <cahilljm@aol.com>
2022-03-08 16:19:10 -0800

    Push function to show or hide fields based on selection

--- contact-me.js
+++ contact-me.js
@@ -1,1 +1,29 @@
-// TODO
+const selector = document.getElementById("contact-reason");
+
+const selectOption = () => {
+    const selection = selector.value;
+    const techfields = document.getElementById("language-type");
+    const jobfields = document.getElementById("job-fields");
+
+   if (selection === "job-opp") {
+        techfields.classList.remove("show");
+        techfields.classList.add("hidden");
+        jobfields.classList.remove("hidden");
+        jobfields.classList.add("show");
+    } else if (selection === "talk-code") {
+        jobfields.classList.remove("show");
+        jobfields.classList.add("hidden");
+        techfields.classList.remove("hidden");
+        techfields.classList.add("show");
+    } else {
+        jobfields.classList.remove("show");
+        techfields.classList.remove("show");
+        jobfields.classList.add("hidden");
+        techfields.classList.add("hidden");
+    }
+
+}
+
+
+
+selector.addEventListener("change", selectOption)
