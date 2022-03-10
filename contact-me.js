abe7760d7407f4d2e8a45b01333768150ec1259a
Arthur Rodrigues <artrodrig3@outlook.com>
2022-03-10 01:11:50 -0800

    Week 7 homework completed.

--- contact-me.js
+++ contact-me.js
@@ -1,1 +1,46 @@
 // TODO
+const webForm = document.getElementById('connect-form');
+const contactKind = document.getElementById('contact-kind');
+const nameField = document.getElementById('name');
+const emailField = document.getElementById('email');
+const codeMessageField = document.getElementById('code-message');
+
+contactKind.addEventListener('change', (e) => {
+    e.preventDefault();
+    const kindValue = e.target.value;
+
+    if (kindValue === 'job-opportunity'){
+        document.getElementById('talk-code').setAttribute('style', 'display: none;');
+        document.getElementById('job-opportunity').removeAttribute('style');
+    }
+    else if (kindValue === 'talk-code') {
+        document.getElementById('job-opportunity').setAttribute('style', 'display: none;');
+        document.getElementById('talk-code').removeAttribute('style');
+    }
+})
+
+const validateForm = (e) => {
+
+    const nameStatus = nameField.value.length > 3;
+    const emailCheck = new RegExp(/\w+@\w+\.\w+/);
+    const emailStatus = emailCheck.exec(emailField.value);
+    const messageStatus = codeMessageField.value.length > 10;
+    
+    if (!nameStatus) {
+        e.preventDefault();
+        alert('Name is not longer than 3 letters. Needs correction');
+    }
+
+    else if (!emailStatus) {
+        e.preventDefault();
+        alert('Email doesn\'t look like an email. Needs to be corrected.');
+    }
+    
+    // TALK CODE SELECTED
+    else if (!messageStatus && contactKind.value === 'talk-code') {
+        e.preventDefault();
+        alert('Message is less than 10 letters long. Make it longer for me to understand.');
+    }
+}
+
+webForm.addEventListener('submit', validateForm );
--- form-validation-custom.js
+++ form-validation-custom.js
@@ -1,1 +1,44 @@
 // TODO
+
+const webForm = document.getElementById('connect-form');
+const firstNameField = document.getElementById('first-name');
+const lastNameField = document.getElementById('last-name');
+const emailField = document.getElementById('email');
+
+const validateForm = (e) => {
+    
+    const firstNameStatus = firstNameField.value.length > 3
+    const lastNameStatus = lastNameField.value.length > 3
+    const emailCheck = new RegExp(/\w+@\w+\.\w+/);
+    const emailStatus = emailCheck.exec(emailField.value);
+    
+    if (!firstNameStatus) {
+        e.preventDefault();
+        firstNameField.parentElement.classList.add('invalid');
+        alert('First name is 3 letters long or fewer. Needs to be longer.');
+        console.log('Bad input.')
+    }
+    else if (!lastNameStatus) {
+        e.preventDefault();
+        lastNameField.parentElement.classList.add('invalid');
+        alert('Last name is 3 letters long or fewer. Needs to be longer.');
+        console.log('Bad input.');
+    }
+    else if (!emailStatus) {
+        e.preventDefault();
+        emailField.parentElement.classList.add('invalid');
+        alert('Email doesn\'t look like an email. Needs to be corrected.');
+        console.log('Bad input.');
+    } 
+
+    else {
+        const fields = [firstNameField, lastNameField, emailField]
+        fields.forEach( field => {
+            if(field.parentElement.classList.contains('invalid')) { 
+                field.parentElement.classList.remove('invalid')
+            }
+        })
+    }
+}
+
+webForm.addEventListener('submit', validateForm);
