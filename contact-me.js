dda765a17996c0caf882a616cb1bf0ca4e97094d
RainyCityDiver <cvmembership@posteo.net>
2022-03-09 17:32:50 -0800

    contact-me code tested and working incl. both extra credit parts.

--- contact-me.js
+++ contact-me.js
@@ -1,6 +1,4 @@
 // TODO
-
-// let form = document.getElementById('contactForm');
 let nameField = document.getElementById('name');
 let emailField = document.getElementById('email');
 let reasonDropDown = document.getElementById('reasonForContact');
@@ -22,12 +20,10 @@ const checkNameLength = function(e) {
     if (nameValidState === false) {
         e.preventDefault();
         nameField.validity.valid = false;
-        nameField.parentElement.classList.add('invalid'); 
         nameField.setCustomValidity(`Your name must be more than three letters.`);
     } else {
         nameField.validity.valid = true;
         nameField.setCustomValidity('');
-        nameField.parentElement.classList.remove('invalid');
     }
     nameField.reportValidity();
 }
@@ -41,12 +37,10 @@ const checkEmail = function(e) {
     if (emailValidState === false) {
         e.preventDefault();
         emailField.validity.valid = false;
-        emailField.parentElement.classList.add('invalid'); 
         emailField.setCustomValidity(`Please ensure your email is properly formatted.`);
     } else {
         emailField.validity.valid = true;
         emailField.setCustomValidity('');
-        emailField.parentElement.classList.remove('invalid');
     }
     nameField.reportValidity();
 }
@@ -54,6 +48,18 @@ const checkEmail = function(e) {
 sendBtn.addEventListener('change', checkEmail, true);
 
 // Contact Reason Code
+const selectOptionZero = function(e) {
+    if (reasonDropDown.value == 'reason') {
+        e.preventDefault();
+        reasonDropDown.validity.valid = false;
+        reasonDropDown.setCustomValidity(`Please make a selection.`);
+    } else if (reasonDropDown.value != 'reason') {
+        reasonDropDown.validity.valid = true;
+        reasonDropDown.setCustomValidity('');
+    }
+    reasonDropDown.reportValidity();
+}
+
 const selectOptionOne = function() {
     let selection = reasonDropDown.value;
     if (selection == "job") {
@@ -73,6 +79,7 @@ const selectOptionTwo = function() {
     }
 }
 
+sendBtn.addEventListener('click', selectOptionZero, true);
 reasonDropDown.addEventListener('change', selectOptionOne, true);
 reasonDropDown.addEventListener('change', selectOptionTwo, true);
 
@@ -84,15 +91,12 @@ const checkJobTitle = function(e) {
         if (jobTitleValidState === false) {
             e.preventDefault();
             jobTitleInputField.validity.valid = false;
-            jobTitleInputField.parentElement.classList.add('invalid'); 
             jobTitleInputField.setCustomValidity(`Please enter the Job's Title.`);
         } else {
             jobTitleInputField.validity.valid = true;
             jobTitleInputField.setCustomValidity('');
-            jobTitleInputField.parentElement.classList.remove('invalid');
         }
     }
-    
     jobTitleInputField.reportValidity();
 }
 
@@ -106,49 +110,27 @@ const checkWebsiteValidity = function(e) {
         if (websiteValidState === false) {
             e.preventDefault();
             websiteInputField.validity.valid = false;
-            websiteInputField.parentElement.classList.add('invalid'); 
             websiteInputField.setCustomValidity(`Please enter a valid website, including 'http://www'.`);
         } else {
             websiteInputField.validity.valid = true;
             websiteInputField.setCustomValidity('');
-            websiteInputField.parentElement.classList.remove('invalid');
         }
     }
-    
     websiteInputField.reportValidity();
 }
 
 sendBtn.addEventListener('click', checkWebsiteValidity, true);
 
-// (Programming) Language validation
-const checkLanguageValidation = function(e) {
-    let languageValidState = reasonDropDown.value;
-    if (languageValidState === false) {
-        reasonDropDown.validity.valid = false;
-        reasonDropDown.parentElement.classList.add('invalid'); 
-        reasonDropDown.setCustomValidity(`Please make a selection.`);
-    } else {
-        reasonDropDown.validity.valid = true;
-        reasonDropDown.setCustomValidity('');
-        reasonDropDown.parentElement.classList.remove('invalid');
-    }
-    reasonDropDown.reportValidity();
-}
-
-sendBtn.addEventListener('change', checkLanguageValidation, true);
-
-// Message validation
+// Message field validation
 const checkMsgLength = function(e) {
     let msgValidState = msgField.value.length > 10;
     if (msgValidState === false) {
         e.preventDefault();
         msgField.validity.valid = false;
-        msgField.parentElement.classList.add('invalid'); 
         msgField.setCustomValidity(`Your message must be more than ten letters.`);
     } else {
         msgField.validity.valid = true;
         msgField.setCustomValidity('');
-        msgField.parentElement.classList.remove('invalid');
     }
     msgField.reportValidity();
 }
@@ -156,16 +138,16 @@ const checkMsgLength = function(e) {
 sendBtn.addEventListener('click', checkMsgLength, true);
 
 
-// Testing
+// This needed to ensure Job Title and Website validation works
 const dropDownSelection = function(e) {
     if (reasonDropDown.value == 'job') {
-        jobTitleInputField.setAttribute('required');
-        websiteInputField.setAttribute('required');
+        jobTitleInputField.setAttribute('required', '');
+        websiteInputField.setAttribute('required', '');
         document.getElementById('language').removeAttribute('required');
     } else if (reasonDropDown.value == 'chat') {
         jobTitleInputField.removeAttribute('required');
         websiteInputField.removeAttribute('required');
-        document.getElementById('language').setAttribute('required');
+        document.getElementById('language').setAttribute('required', 'required');
     }
 }
 
