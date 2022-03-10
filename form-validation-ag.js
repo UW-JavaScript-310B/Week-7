5488bf36e76e4241101d0cc729749d733a81d149
aliciagoodwin <alicia@spiltmilkware.com>
2022-03-09 20:09:41 -0800

    custom contact form

--- form-validation-ag.js
+++ form-validation-ag.js
@@ -3,14 +3,14 @@ const nameField = document.getElementById("name");
 const emailAddress = document.getElementById("email");
 const validEmail = /\w+@\w+\.\w+/;
 const reasonForContact = document.getElementById("contact-reason");
-const message = document.getElementById("messsage");
+const message = document.getElementById("message");
 const form = document.getElementById("connect-form");
+
 
 const checkValidation = (e) => {
  
   if (nameField.value.length < 3) {
     e.preventDefault();
- 
     nameField.setCustomValidity("Your name must be longer than 3 letters");
     nameField.reportValidity();
     nameField.parentElement.classList.add("invalid");
@@ -20,7 +20,7 @@ const checkValidation = (e) => {
     nameField.parentElement.classList.add("valid");
   } // value of name field less than 3
 
- 
+
     if (!validEmail.test(emailAddress)) {
       emailAddress.reportValidity();
       emailAddress.parentElement.classList.add("invalid");
@@ -29,19 +29,31 @@ const checkValidation = (e) => {
         emailAddress.setCustomValidity("");
 
     } //valid email
+   // debugger;
+    if (reasonForContact.value === "choose") {
+        reasonForContact.setCustomValidity("please select a reason for contacting us");
+        reasonForContact.parentElement.classList.add('invalid');
+    } else {
+        reasonForContact.setCustomValidity("");
+        reasonForContact.parentElement.classList.remove('invalid');
+        reasonForContact.parentElement.classList.add('valid');
+      }
 
     if (message) {
          if (message.value.length < 10) {
           e.preventDefault();
           message.setCustomValidity("Your message needs to be longer than 10 characters");
-    } 
-}
+        } else {
+        message.setCustomValidity("");
+
+    }// message value length
+} //does message exist
   
 }; //check validation function
 
 const selectReasonForContact = (e) => {
   const selection = reasonForContact.value;
-  
+  //debugger;
   const jobReason = document.getElementById("job");
   const jobCompany = document.getElementById("company");
   const talkCodeReason = document.getElementById("talk-code");
@@ -49,13 +61,18 @@ const selectReasonForContact = (e) => {
   if (selection === "job") {
    jobReason.classList.remove("hidden");
    jobCompany.classList.remove("hidden");
-    talkCodeReason.classList.add("hidden");
+   jobCompany.children[1].setAttribute("required","");
+   jobReason.children[1].setAttribute("required", "");
+  
+   talkCodeReason.classList.add("hidden");
   } else if (selection === "talk-code") {
     talkCodeReason.classList.remove("hidden");
     jobReason.classList.add("hidden");
     jobCompany.classList.add("hidden");
+    jobCompany.children[1].removeAttribute("required");
+    jobReason.children[1].removeAttribute("required");
 
-} //what is selection
+    } //what is selection
 }; // reason for contact
 
 form.addEventListener("submit", checkValidation);
--- form-validation-custom.js
+++ form-validation-custom.js
@@ -1,1 +1,65 @@
 // TODO
+const firstNameField = document.getElementById("first-name");
+const lastNameField = document.getElementById("last-name");
+const emailAddress = document.getElementById("email");
+const validEmail =  /\w+@\w+\.\w+/;
+const form = document.getElementById("connect-form")
+
+const nameFields = [firstNameField, lastNameField];
+
+const checkValidation = (e) => {
+ 
+for (let index = 0; index < nameFields.length; index++) {
+    const theName = nameFields[index];
+    if ( theName.value.length < 3 ) {
+        e.preventDefault()
+        theName.setCustomValidity("Your name must be longer than 3 letters")
+        theName.parentElement.classList.add('invalid');
+        theName.parentElement.classList.remove('valid');
+
+    } else {
+        theName.validity.valid = true;
+        theName.parentElement.classList.remove('invalid')
+        theName.parentElement.classList.add('valid')
+
+    } // value of name field less than 3
+} //for loop
+
+if (!validEmail.test(emailAddress)) {
+    e.preventDefault()
+    emailAddress.setCustomValidity("Please check your email address")
+    emailAddress.parentElement.classList.add('invalid');
+    emailAddress.parentElement.classList.remove('valid');
+   
+    } else {
+        
+    emailAddress.validity.valid = true;
+    emailAddress.parentElement.classList.remove('invalid')
+    emailAddress.parentElement.classList.add('valid')
+
+
+   } 
+
+
+
+
+} //check validation function
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+form.addEventListener("submit", checkValidation);
