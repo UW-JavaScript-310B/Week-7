aded66471f3d35918ca04cf1da8b4870c7e1ca30
Admin <admin@Admins-MacBook-Pro.local>
2022-03-10 11:41:35 -0800

    Complete refactoring to eliminate repetitive code.

--- contact-me.js
+++ contact-me.js
@@ -8,51 +8,79 @@ const companyUrlField = document.getElementById('company-url');
 const codingLanguageField = document.getElementById('coding-language');
 const messageField = document.getElementById('message');
 
+
+const validationRules = {
+  'first-name': {
+    id: 'first-name',
+    minLength: 3,
+    pattern: new RegExp(/./),
+    validation: 'A minimum of three characters are required',
+    required: true
+  },
+  'last-name': {
+    id: 'last-name',
+    minLength: 3,
+    pattern: new RegExp(/./),
+    validation: 'A minimum of three characters are required',
+    required: true
+  },
+  email: {
+    id: 'email',
+    minLength: 1,
+    pattern: new RegExp(/\w+@\w+\.\w+/),
+    validation: 'Please enter a valid email address',
+    required: true
+  },
+  'job-title': {
+    id: 'job-title',
+    minLength: 1,
+    pattern: new RegExp(/./),
+    validation: 'Please enter your job title}',
+    required: false
+  },
+  'company-url': {
+    id: 'company-url',
+    minLength: 1,
+    pattern: new RegExp(/https?:\/\/.+\..+/),
+    validation: 'A valid web address is required',
+    required: false
+  },
+  'coding-language': {
+    id: 'coding-language',
+    minLength: 1,
+    pattern: new RegExp(/./),
+    validation: 'Please enter or choose a coding language',
+    required: false
+  },
+  message: {
+    id: 'message',
+    minLength: 10,
+    pattern: new RegExp(/./),
+    validation: 'A minimum of ten characters are required',
+    required: true
+  }
+}
+
 /**
  * Upon submit event, validate first name, last name, email, and textarea fields
  * @param {Event object} e 
  */
 const submissionValidation = (e) => {
-  if(firstNameField.value.trim().length < 3) {
-    firstNameField.setCustomValidity(`A minimum of three characters are required`);
-    firstNameField.reportValidity();
-    e.preventDefault();
-  }
-  else {
-    firstNameField.setCustomValidity('');
-    firstNameField.reportValidity();
-  }
 
-  if(lastNameField.value.trim().length < 3) {
-    lastNameField.setCustomValidity(`A minimum of three characters are required`);
-    lastNameField.reportValidity();
-    e.preventDefault();
-  }
-  else {
-    lastNameField.setCustomValidity('');
-    lastNameField.reportValidity();
-  }
-
-  if(!(/\w+@\w+\.\w+/).test(emailField.value.trim())) {
-    emailField.setCustomValidity(`Please enter a valid email address`);
-    emailField.reportValidity();
-    e.preventDefault();
-  }
-  else {
-    emailField.setCustomValidity('');
-    emailField.reportValidity();
-  }
-
-  if(messageField.value.trim().length < 10) {
-    messageField.setCustomValidity(`A minimum of 10 characters is required`);
-    messageField.reportValidity();
-    e.preventDefault();
-  }
-  else {
-    messageField.setCustomValidity('');
-    messageField.reportValidity();
-  }
-  form.reportValidity();
+  Object.keys(validationRules).forEach(key => {
+    
+    const element = document.getElementById(validationRules[key].id);
+    const elementValue = element.value.trim();
+    if(element.required && (elementValue.length < validationRules[key].minLength || !validationRules[key].pattern.test(elementValue))) {
+      element.setAttribute('aria-invalid', 'true');
+      element.setCustomValidity(validationRules[key].validation);
+      e.preventDefault();
+    }
+    else {
+      element.setCustomValidity('');
+    }
+    form.reportValidity();
+  })
 }
 
 /**
@@ -60,44 +88,65 @@ const submissionValidation = (e) => {
  * @param {Event object} e 
  */
 const formRealTimeFeedback = (e) => {
-  // set "changed" class to utilize associated CSS styles
-  e.target.classList.add('changed');
-  
-  if(e.target.name.includes('name') && e.target.value.trim().length < 3) {
-    e.target.setCustomValidity(`A minimum of three characters are required`);
-  }
-  else if(e.target.name.includes('email') && !(/\w+@\w+\.\w+/).test(e.target.value.trim())) {
-    e.target.setCustomValidity(`Please enter a valid email address`);
-  }
-  else if(e.target.name.includes('message') && e.target.value.trim().length < 10) {
-    e.target.setCustomValidity(`A minimum of ten characters are required`);
+  const element = e.target;
+  const elementValue = element.value.trim();
+
+  console.log(e.target.id);
+
+  if(element.required && (elementValue.length < validationRules[e.target.id].minLength || !validationRules[e.target.id].pattern.test(elementValue))) {
+    e.target.setCustomValidity(validationRules[e.target.id].validation);
+    e.target.setAttribute("aria-invalid", "true");
   }
   else {
     e.target.setCustomValidity('');
+    e.target.setAttribute("aria-invalid", "false");
   }
   e.target.reportValidity();
 }
 
 /**
- * Toggles hidden form fields when parent select options are chosen
+ * Shows or hides form fields when parent select options are chosen
  */
  const selectUpdate = () => {
   if(reasonField.value === 'job') {
-    jobTitleField.required = true;
-    jobTitleField.parentElement.classList.remove('hidden');
-    companyUrlField.required = true;
-    companyUrlField.parentElement.classList.remove('hidden');
-    codingLanguageField.required = false;
-    codingLanguageField.parentElement.classList.add('hidden');
+    setRequiredField(jobTitleField, true);
+    updateCssClass(jobTitleField.parentElement, 'remove', 'hidden');
+    
+    setRequiredField(companyUrlField, true);
+    updateCssClass(companyUrlField.parentElement, 'remove', 'hidden');
+
+    setRequiredField(codingLanguageField, false);
+    updateCssClass(codingLanguageField.parentElement, 'add', 'hidden');
   }
   else if(reasonField.value === 'code') {
-    codingLanguageField.required = true;
-    codingLanguageField.parentElement.classList.remove('hidden');
-    jobTitleField.required = false;
-    jobTitleField.parentElement.classList.add('hidden');
-    companyUrlField.required = false;
-    companyUrlField.parentElement.classList.add('hidden');
+    setRequiredField(codingLanguageField, true);
+    updateCssClass(codingLanguageField.parentElement, 'remove', 'hidden');
+    
+    setRequiredField(jobTitleField, false);
+    updateCssClass(jobTitleField.parentElement, 'add', 'hidden');
+
+    setRequiredField(companyUrlField, false);
+    updateCssClass(companyUrlField.parentElement, 'add', 'hidden');
   }
+}
+
+/**
+ * Helper function to set boolearn required attribute on HTML elements
+ * @param {HTML Elerment} element 
+ * @param {boolean} state 
+ */
+const setRequiredField = (element, state) => {
+  element.required = state;
+}
+
+/**
+ * Helper function to add/remove CSS classes from HTML elements
+ * @param {HTML Element} element 
+ * @param {string} action 
+ * @param {string} cssClass 
+ */
+const updateCssClass = (element, action, cssClass) => {
+  element.classList[action](cssClass);
 }
 
 form.addEventListener('input', formRealTimeFeedback);
