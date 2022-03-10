0a0e27636eb081fec5937ad7f536c4d449328a3f
Nell B <nellb28@yahoo.com>
2022-03-09 20:27:50 -0600

    Initial black jack tests

---
+++ blackjack.spec.js
@@ -0,0 +1,14 @@
+describe("Tests for Black Jack", () => {
+  describe("Tests for card scenarios", () => {
+    //– No aces
+    it("Should return", () => {});
+    //– 1 ace case (soft)
+    it("Should return", () => {});
+    //– 1 ace case (not soft)
+    it("Should return", () => {});
+    //– multipe ace case (soft)
+    it("Should return", () => {});
+    //– multipe ace case (not soft)
+    it("Should return", () => {});
+  });
+});
