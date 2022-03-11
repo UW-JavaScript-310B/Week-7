f2c8998a415cac28910a726a48809486544baac9
Ryan Ku <rjhk@uw.edu>
2022-03-10 17:48:59 -0800

    homework complete

--- constraint-validation.js
+++ constraint-validation.js
@@ -1,1 +1,29 @@
 // TODO
+const nameField = document.getElementById('first-name');
+const form = document.getElementById('connect-form');
+const selector = document.getElementById('contact-kind');
+
+const selectOption = (e) => {
+    const selection = selector.value;
+    const bizFields = document.createElement('biz-name');
+
+    if (selection === 'business') {
+        bizFields.classList.remove('hidden');
+        bizFields.classList.show('show');
+    } else if (selection === 'technical') {
+        bizFields.classList.show('hidden');
+    }
+}
+
+const checkValidation = (e) => {
+    const validState = (nameField.value.length > 1);
+    if (!validState) {
+        e.preventDefault();
+        nameField.setCustomValidity('Your first name must be more than 1 letter');
+    } else {
+        nameField.validity.valid = true; 
+    }
+}
+
+form.addEventListener('submit', checkValidation);
+selector.addEventListener('change', selectOption);
--- form-validation-custom.js
+++ form-validation-custom.js
@@ -1,1 +1,20 @@
 // TODO
+const nameField = document.getElementById('first-name');
+const form = document.getElementById('connect-form invalid');
+
+const checkValidation = (e) => {
+    const validState = (nameField.value.length > 1);
+    if (!validState) {
+        e.preventDefault();
+        nameField.setCustomValidity('Your first name must be more than 1 letter');
+        // nameField.closest('form-group').classList.remove('valid');
+        // nameField.closest('form-group').classList.add('invalid');
+        console.log('bad input');
+    } else {
+        nameField.validity.valid = true; 
+        // nameField.closest('form-group').classList.remove('invalid');
+        // nameField.closest('form-group').classList.add('valid');
+    }
+}
+
+form.addEventListener('submit', checkValidation);
