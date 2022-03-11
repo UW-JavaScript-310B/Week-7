eab207f61b797a209058eaa4331268605dcc3f53
Nell B <nellb28@yahoo.com>
2022-03-10 18:20:46 -0600

    Cleaning up the constraint validaiton stuff. Tired of playing around.

--- constraint-validation.js
+++ constraint-validation.js
@@ -1,15 +1,15 @@
-// TODO
-const firstNameField = document.getElementById("first-name");
 const form = document.getElementById("connect-form");
-/*
+
 const checkValidation = (e) => {
   //debugger;
-  console.log(firstNameField.value.length);
+  validateFirstName(e);
+  validateLastName(e);
+};
+
+function validateFirstName(e) {
+  const firstNameField = document.getElementById("first-name");
   const validState = firstNameField.value.length > 3;
   const formGrp = firstNameField.parentElement;
-  firstNameField.setCustomValidity("");
-
-  console.log(firstNameField.parentElement);
 
   if (!validState) {
     e.preventDefault();
@@ -27,54 +27,27 @@ const checkValidation = (e) => {
     firstNameField.validity.valid = true;
     firstNameField.reportValidity;
   }
-};
+}
 
-form.addEventListener("submit", checkValidation);
-//TODO - there is an issue when we refresh the page. Need to investigate this
-*/
-
-/*
-const checkValidation = () => {
-  //debugger;
-  console.log(firstNameField.value.length);
-  const validState = firstNameField.value.length > 3;
-  const formGrp = firstNameField.parentElement;
-
-  console.log(firstNameField.parentElement);
+function validateLastName(e) {
+  const lastNameField = document.getElementById("last-name");
+  const validState = lastNameField.value.length > 3;
+  const formGrp = lastNameField.parentElement;
 
   if (!validState) {
+    e.preventDefault();
     formGrp.classList.add("invalid");
     formGrp.classList.remove("valid");
-    firstNameField.validity.valid = false;
-    firstNameField.setCustomValidity(
-      "First name must be greater than 3 characters!"
+    lastNameField.setCustomValidity(
+      "Last name must be greater than 3 characters!"
     );
-    firstNameField.reportValidity;
+    lastNameField.validity.valid = false;
+    lastNameField.reportValidity;
   } else {
     formGrp.classList.remove("invalid");
     formGrp.classList.add("valid");
-    firstNameField.validity.valid = true;
-    firstNameField.setCustomValidity("");
-    firstNameField.reportValidity;
+    lastNameField.setCustomValidity("");
+    lastNameField.validity.valid = true;
+    lastNameField.reportValidity;
   }
-
-  console.log(validState);
-  return validState;
-};
-
-/*
-    if (!validFirstName) {
-        console.log("Bad Input");
-        parentDivFirstName.classList.remove("valid");
-        parentDivFirstName.classList.add("invalid");
-        firstName.setCustomValidity("Your first name must include at least 3 characters");
-        firstName.validity.valid = false;
-        firstName.reportValidity();
-    } else {
-        parentDivFirstName.classList.remove("invalid");
-        parentDivFirstName.classList.add("valid");
-        firstName.setCustomValidity("");
-        firstName.validity.valid = true;
-        firstName.reportValidity();
-    }
-    */
+}
