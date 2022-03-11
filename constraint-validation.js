dc7e441305a349f6f6b3d10c85477546168146e9
Nell B <nellb28@yahoo.com>
2022-03-10 20:24:36 -0600

    Add email field validation. Add cleanup

--- constraint-validation.js
+++ constraint-validation.js
@@ -4,14 +4,21 @@ const checkValidation = (e) => {
   //debugger;
   validateFirstName(e);
   validateLastName(e);
+  validateTestEmail(e);
 };
 
 function validateFirstName(e) {
   const firstNameField = document.getElementById("first-name");
-  const validState = firstNameField.value.length > 3;
   const formGrp = firstNameField.parentElement;
+  const validState = firstNameField.value.length > 3;
 
-  if (!validState) {
+  if (validState) {
+    formGrp.classList.add("valid");
+    formGrp.classList.remove("invalid");
+    firstNameField.setCustomValidity("");
+    firstNameField.validity.valid = true;
+    firstNameField.reportValidity;
+  } else {
     e.preventDefault();
     formGrp.classList.add("invalid");
     formGrp.classList.remove("valid");
@@ -20,21 +27,21 @@ function validateFirstName(e) {
     );
     firstNameField.validity.valid = false;
     firstNameField.reportValidity;
-  } else {
-    formGrp.classList.remove("invalid");
-    formGrp.classList.add("valid");
-    firstNameField.setCustomValidity("");
-    firstNameField.validity.valid = true;
-    firstNameField.reportValidity;
   }
 }
 
 function validateLastName(e) {
   const lastNameField = document.getElementById("last-name");
-  const validState = lastNameField.value.length > 3;
   const formGrp = lastNameField.parentElement;
+  const validState = lastNameField.value.length > 3;
 
-  if (!validState) {
+  if (validState) {
+    formGrp.classList.add("valid");
+    formGrp.classList.remove("invalid");
+    lastNameField.setCustomValidity("");
+    lastNameField.validity.valid = true;
+    lastNameField.reportValidity;
+  } else {
     e.preventDefault();
     formGrp.classList.add("invalid");
     formGrp.classList.remove("valid");
@@ -43,11 +50,29 @@ function validateLastName(e) {
     );
     lastNameField.validity.valid = false;
     lastNameField.reportValidity;
-  } else {
-    formGrp.classList.remove("invalid");
-    formGrp.classList.add("valid");
-    lastNameField.setCustomValidity("");
-    lastNameField.validity.valid = true;
-    lastNameField.reportValidity;
   }
 }
+
+function validateTestEmail(e) {
+  const emailField = document.getElementById("email");
+  const formGrp = emailField.parentElement;
+  const regEx = new RegExp("\\w+@\\w+.\\w+");
+  const isEmailValid = regEx.test(emailField.value);
+
+  if (isEmailValid) {
+    formGrp.classList.add("valid");
+    formGrp.classList.remove("invalid");
+    emailField.setCustomValidity(" ");
+    emailField.validity.valid = true;
+    emailField.reportValidity();
+  } else {
+    e.preventDefault();
+    formGrp.classList.add("invalid");
+    formGrp.classList.remove("valid");
+    emailField.setCustomValidity("Please enter a valid email address.");
+    emailField.validity.valid = false;
+    emailField.reportValidity();
+  }
+}
+
+form.addEventListener("submit", checkValidation);
