84ffaa0ac82d99c1dd116a2e6c22dc8e348fbb20
Nell B <nellb28@yahoo.com>
2022-03-12 07:16:14 -0600

    Add more logic for missing form validation. Cleanup code.

--- constraint-validation.js
+++ constraint-validation.js
@@ -1,16 +1,44 @@
-const form = document.getElementById("connect-form");
+const signUpButton = document.getElementsByClassName("btn btn-primary")[0];
+const selector = document.getElementById("contact-kind");
 
-const checkValidation = (e) => {
+selector.addEventListener("change", selectOption);
+signUpButton.addEventListener("click", checkValidation);
+
+function selectOption() {
+  const selection = selector.value;
+  const bizNameFields = document.getElementById("biz-name");
+  const techNameFields = document.getElementById("tech-type");
+
+  if (selection === "business") {
+    bizNameFields.classList.add("show");
+    bizNameFields.classList.remove("hidden");
+    techNameFields.classList.remove("show");
+    techNameFields.classList.add("hidden");
+  } else if (selection === "technical") {
+    techNameFields.classList.add("show");
+    techNameFields.classList.remove("hidden");
+    bizNameFields.classList.add("hidden");
+    bizNameFields.classList.remove("show");
+  } else {
+    bizNameFields.classList.add("hidden");
+    techNameFields.classList.add("hidden");
+    bizNameFields.classList.remove("show");
+    techNameFields.classList.remove("show");
+  }
+}
+
+function checkValidation(e) {
   //debugger;
   validateFirstName(e);
   validateLastName(e);
   validateTestEmail(e);
-};
+}
 
 function validateFirstName(e) {
   const firstNameField = document.getElementById("first-name");
   const formGrp = firstNameField.parentElement;
   const validState = firstNameField.value.length > 3;
+  console.log(`first name length ${firstNameField.value.length}`);
 
   if (validState) {
     formGrp.classList.add("valid");
@@ -19,7 +47,7 @@ function validateFirstName(e) {
     firstNameField.validity.valid = true;
     firstNameField.reportValidity;
   } else {
-    e.preventDefault();
+    //e.preventDefault();
     formGrp.classList.add("invalid");
     formGrp.classList.remove("valid");
     errorString = "First name must be greater than 3 characters!";
@@ -34,6 +62,7 @@ function validateLastName(e) {
   const lastNameField = document.getElementById("last-name");
   const formGrp = lastNameField.parentElement;
   const validState = lastNameField.value.length > 3;
+  console.log(`last name length ${lastNameField.value.length}`);
 
   if (validState) {
     formGrp.classList.add("valid");
@@ -42,7 +71,7 @@ function validateLastName(e) {
     lastNameField.validity.valid = true;
     lastNameField.reportValidity;
   } else {
-    e.preventDefault();
+    //e.preventDefault();
     formGrp.classList.add("invalid");
     formGrp.classList.remove("valid");
     errorString = "Last name must be greater than 3 characters!";
@@ -66,7 +95,7 @@ function validateTestEmail(e) {
     emailField.validity.valid = true;
     emailField.reportValidity();
   } else {
-    e.preventDefault();
+    //e.preventDefault();
     formGrp.classList.add("invalid");
     formGrp.classList.remove("valid");
     errorString = "Please enter a valid email address.";
@@ -76,5 +105,3 @@ function validateTestEmail(e) {
     emailField.reportValidity();
   }
 }
-
-form.addEventListener("submit", checkValidation);
