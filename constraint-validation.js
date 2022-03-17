8f2cbccdb56cc6ae0fcb2a44385160a59fa31236
rick <rick@rdaltonphotos.com>
2022-03-17 00:00:57 -0700

    Rick Dalton HW 7 Complete

--- constraint-validation.js
+++ constraint-validation.js
@@ -1,1 +1,25 @@
-// TODO
+
+const nameField = document.getElementById("first-name")
+
+const form = document.getElementById("connect-form")
+
+const checkValidation = (e) =>
+{
+  const validState = nameField.value.length > 3
+  if (!validState)
+  {
+    e.preventDefault()
+    nameField.setCustomValidity("First Name must be more than 3 letters")
+    nameField.closest('form-group').classList.remove('valid')
+    nameField.closest("form-group").classList.add('invalid')
+  }
+  else
+  {
+    nameField.validity.valid = true;
+    nameField.closest("form-group").classList.remove('invalid')
+    nameField.closest("form-group").classList.add('valid')
+  }
+  console.log(nameField.value)
+}
+
+form.addEventListener('submit', checkValidation)
--- form-validation-custom.js
+++ form-validation-custom.js
@@ -1,1 +1,86 @@
-// TODO
+function setInvalid(field)
+{
+    let nearestDiv = field.closest('div.form-group')
+    nearestDiv.classList.remove('valid')
+    nearestDiv.classList.add('invalid')
+}
+
+function setValid(field)
+{
+    let nearestDiv = field.closest('div.form-group')
+    nearestDiv.classList.remove('invalid')
+    nearestDiv.classList.add('valid')
+}
+
+function validateTextFieldLength(field, minLength)
+{
+    return field.value.length >= minLength
+}
+
+function validateTextFieldPattern(field, pattern)
+{
+    const re = new RegExp(pattern, 'g')
+    return re.test(field.value)
+}
+
+function checkName(field, nameType)
+{
+    result = true
+    if (!validateTextFieldLength(field, 4))
+    {
+        field.setCustomValidity(`${nameType} Name must be more than 3 letters`)
+        field.reportValidity()
+        setInvalid(field);
+        console.log(`Bad Input: ${field.value}`)
+        result = false
+    }
+    else
+    {
+        field.validity.valid = true
+        setValid(field)
+    }
+
+    return result
+}
+
+function checkEmail(field, pattern)
+{
+    result = true
+    if (!validateTextFieldPattern(field, pattern))
+    {
+        field.setCustomValidity(`Email needs to be of the form xxx@yyy.zzz`)
+        field.reportValidity()
+        setInvalid(field);
+        console.log(`Bad Input: ${field.value}`)
+        result = false
+    }
+    else
+    {
+        field.validity.valid = true
+        setValid(field)
+    }
+
+    return result
+}
+
+const checkValidation = (e) =>
+{
+    const fNameField = document.getElementById("first-name")
+    const lNameField = document.getElementById("last-name")
+    const emailField = document.getElementById("email")
+    const emailPattern = '\\w+@\\w+\\.\\w+'
+    let fNameValid = checkName(fNameField, "First")
+    let lNameValid = checkName(lNameField, "Last")
+    let emailValid = checkEmail(emailField, emailPattern)
+    const validState = fNameValid
+        && lNameValid
+        && emailValid
+
+    if (!validState)
+    {
+        e.preventDefault()
+    }
+}
+
+const form = document.getElementById("connect-form")
+form.addEventListener('submit', checkValidation)
