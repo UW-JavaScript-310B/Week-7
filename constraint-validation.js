d648a126340d6df8077e2e15cbdb483e875fa38c
Ryan Ku <rjhk@uw.edu>
2022-03-21 16:38:26 -0700

    hw complete

--- constraint-validation.js
+++ constraint-validation.js
@@ -1,7 +1,11 @@
 // TODO
 const nameField = document.getElementById('first-name');
-const form = document.getElementById('connect-form');
+const nameField2 = document.getElementById('last-name');
+const form = document.querySelector('.btn');
 const selector = document.getElementById('contact-kind');
+
+const emailField = document.getElementById('email');
+const messageField = document.getElementById('box');
 
 const selectOption = (e) => {
     const selection = selector.value;
@@ -15,15 +19,47 @@ const selectOption = (e) => {
     }
 }
 
-const checkValidation = (e) => {
-    const validState = (nameField.value.length > 1);
-    if (!validState) {
+const checkFirstName = (e) => {
+    const validStateName = (nameField.value.length > 3);
+    if (!validStateName) {
         e.preventDefault();
-        nameField.setCustomValidity('Your first name must be more than 1 letter');
-    } else {
-        nameField.validity.valid = true; 
+        nameField.setCustomValidity('Your name must be more than 3 characters');
+        console.log('Bad input');
+    } else if (validStateName) {
+        nameField.setCustomValidity('')
     }
+    nameField.reportValidity();
 }
 
-form.addEventListener('submit', checkValidation);
+const checkSecondName = (e) => {
+    const validStateName = (nameField2.value.length > 3);
+    if (!validStateName) {
+        e.preventDefault();
+        nameField2.setCustomValidity('Your name must be more than 3 characters');
+        console.log('Bad input');
+    } else if (validStateName) {
+        nameField2.setCustomValidity('')
+    }
+    nameField2.reportValidity();
+}
+
+const checkEmail = (e) => {
+    const validStateEmail = /\w+@\w+\.\w+/.test(emailField.value);
+    if (!validStateEmail) {
+        e.preventDefault();
+        emailField.setCustomValidity('This is not a valid email address');
+        console.log('Bad input');
+    } else {
+        emailField.setCustomValidity('')
+    }
+    emailField.reportValidity();
+}
+
+const checkValidation = (e) => {
+    checkFirstName(e);
+    checkSecondName(e);
+    checkEmail(e);
+}
+
+form.addEventListener('click', checkValidation);
 selector.addEventListener('change', selectOption);
