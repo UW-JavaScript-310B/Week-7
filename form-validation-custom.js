d02a826007bdb0a98b1d6bc1ef6fdf3183925ed9
arturbabajanyan <arturbabajanyanv@gmail.com>
2023-12-01 22:48:00 -0500

    last week homework finished

--- form-validation-custom.js
+++ form-validation-custom.js
@@ -2,48 +2,56 @@
 const formEl = document.getElementById("connect-form");
 const firstName = document.getElementById("first-name");
 const lastName = document.getElementById("last-name");
-const mailEl = document.getElementById("email");
+const emailEl = document.getElementById("email");
 const reg = /^([a-z\d\.-_]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]+)?$/;
 
-const submitValidation = (input, minlength) => {
-    if (input.value.trim().length >= minlength) {
-        input.classList.add("valid");
-        return true;
+const minLength = 3;
+ 
+const textAreaValidation = function(input, minLength) {
+    if (input.value.length < minLength) {
+        input.classList.add('invalid');
+        input.classList.remove('valid');
+    } else {
+        input.classList.add('valid');
+        input.classList.remove('invalid');
     }
-    else{
-        input.classList.add("invalid");
-        alert('Please enter at least 3 characters for the first and last names');
-        if (lastName.value.length <= 3) {
-            lastName.classList.add("invalid");
-        } else {
-            lastName.classList.add("valid");
-        }
-        
-        if (!reg.test(email.value.trim())) {
-            console.log();
-            mailEl.classList.add("invalid");
-        }
+ 
+    if (input.classList.contains('valid')) {
+        return true;
+    } else {
         return false;
     }
 };
-const emailValidation = (email) => {
-        if (reg.test(email.value.trim())){
-                email.pclassList.add("valid");
-                return true;
-        }
-        else {
-                email.classList.add("invalid");
-                alert("Please enter correct email address");
-                return false;
-        }
-}
+ 
+const emailValidation = function(emailcheck) {
+    if (!reg.test(emailcheck.value.trim())) {
+        emailcheck.classList.add('invalid');
+        emailcheck.classList.remove('valid');
+    } else {
+        emailcheck.classList.add('valid');
+        emailcheck.classList.remove('invalid');
+    }
+ 
+    if (emailEl.classList.contains('valid')) {
+        return true;
+    } else {
+        return false;
+    }
+};
 
-formEl.addEventListener("submit",(e) => {
-        if (submitValidation(firstName, 3) && submitValidation(lastName, 3) && emailValidation(mailEl)){
-            alert('Success');
-        }
-        else {
-            alert("Bad Output");
-            e.preventDefault();
-        }
+firstName.addEventListener("change", () => textAreaValidation(firstName, minLength));
+lastName.addEventListener("change", () => textAreaValidation(lastName, minLength));
+emailEl.addEventListener("change", () => emailValidation(emailEl));
+ 
+formEl.addEventListener('submit', (e) => {
+    if (textAreaValidation(firstName, minLength) && textAreaValidation(lastName, minLength) && emailValidation(emailEl)) {
+        alert('Success!!!');
+        // e.preventDefault();
+    } else {
+        textAreaValidation(firstName, minLength);
+        textAreaValidation(lastName, minLength);
+        emailValidation(emailEl);
+        console.log("Bad Output");
+        e.preventDefault();
+    }
 })
